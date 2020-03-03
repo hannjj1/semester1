a400de2d547d0acd46473c2f703ce710b52394b4
Jacob Hannigan (1000062029) <HANNJJ1@student.op.ac.nz>
2020-03-03 14:49:47 +1300

    Friend Class

---
+++ Programming 3/Java/PFriend/src/pfriend/Gender.java
@@ -0,0 +1,7 @@
+package pfriend;
+
+public enum Gender {
+	MALE,
+	FEMALE,
+	NON_BINARY
+}
---
+++ Programming 3/Java/PFriend/src/pfriend/Relationship.java
@@ -0,0 +1,15 @@
+package pfriend;
+
+public enum Relationship {
+	CIVIL_UNION,
+	COMPLICATED,
+	DIVORCED,
+	ENGAGED,
+	MARRIED, 
+	NOT_SPECIFIED,
+	OPEN,
+	RELATIONSHIP,
+	SEPERATED,
+	SINGLE,
+	WIDOWED
+}
